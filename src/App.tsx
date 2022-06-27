1e9980987b1cda5160e41d8a5182d16a07a96f30
Aleksandr Belozerov <allbel900@gmail.com>
2022-06-27 23:35:33 +0300

    Hook useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,51 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TodoList, {TaskType} from "./TodoList";
+
+export type FilterValuesType = 'all' | 'active' | 'completed'
 
 function App() {
     const title_1 = "What to learn";
     const title_2 = "What to learn";
 
-    const tasks_1: Array<TaskType> = [
+    const [tasks, setTasks] = useState<Array<TaskType>>([
         {id: 1, title: "HTML", isDone: true},
         {id: 2, title: "CSS", isDone: true},
-        {id: 3, title: "JS/ES6", isDone: false},
-    ];
-
-    const tasks_2: Array<TaskType> = [
-        {id: 1, title: "React", isDone: true},
-        {id: 2, title: "Vue", isDone: false},
         {id: 3, title: "Angular", isDone: false},
-    ];
+    ])
+
+    const removeTask = (taskID: number) => {
+        setTasks(tasks.filter(task => task.id !== taskID))
+    }
+
+    const [filter, setFilter] = useState<FilterValuesType>('all');
+
+    const changeFilter = (filter: FilterValuesType) => {
+        setFilter(filter)
+    }
+
+    let tasksForRender;
+
+    switch (filter) {
+        case 'completed':
+            tasksForRender = tasks.filter(t => t.isDone === true)
+            break
+        case 'active':
+            tasksForRender = tasks.filter(t => t.isDone === false)
+            break
+        default:
+            tasksForRender = tasks
+    }
 
     return (
         <div className="App">
-            <TodoList title={title_1} tasks={tasks_1} />
-            <TodoList title={title_2} tasks={tasks_2} />
+            <TodoList
+                title={title_1}
+                tasks={tasksForRender}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
+            {/*<TodoList title={title_2} tasks={tasks_2} />*/}
         </div>
     );
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FilterValuesType} from "./App";
 
 
 export type TaskType = {
@@ -10,9 +11,27 @@ export type TaskType = {
 type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (taskID: number) => void
+    changeFilter: (filter: FilterValuesType) => void
 }
 
 const TodoList = (props: TodoListPropsType) => {
+
+    const tasksListItems = props.tasks.length
+
+        ? props.tasks.map(task => {
+
+            const removeTask = () => props.removeTask(task.id)
+
+            return (
+                <li>
+                    <input type="checkbox" checked={task.isDone} />
+                    <span>{task.title}</span>
+                    <button onClick={removeTask}>x</button>
+                </li>
+            )
+        }) : <span>TaskList is empty</span>
+
     return (
         <div>
             <h3>{props.title}</h3>
@@ -22,24 +41,13 @@ const TodoList = (props: TodoListPropsType) => {
             </div>
 
             <ul>
-                <li>
-                    <input type="checkbox" checked={props.tasks[0].isDone} />
-                    <span>{props.tasks[0].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[1].isDone} />
-                    <span>{props.tasks[1].title}</span>
-                </li>
-                <li>
-                    <input type="checkbox" checked={props.tasks[2].isDone} />
-                    <span>{props.tasks[2].title}</span>
-                </li>
+                {tasksListItems}
             </ul>
 
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={ () => props.changeFilter('all') }>All</button>
+                <button onClick={ () => props.changeFilter('active') }>Active</button>
+                <button onClick={ () => props.changeFilter('completed') }>Completed</button>
             </div>
         </div>
     );
