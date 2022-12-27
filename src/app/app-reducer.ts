81fc93570cb9351e28c27720371c458030b44bc2
Aleksandr Belozerov <allbel900@gmail.com>
2022-12-28 01:45:59 +0300

    refactor: app reducer thunks

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,10 +1,24 @@
-import {Dispatch} from "redux";
 import {authAPI} from "../api/todolist-api";
 import {setIsLoggedInAC} from "../features/Login/auth-reducer";
-import {handleServerAppError, handleServerNetworkError} from "../utils/error-utils";
-import {AxiosError} from "axios";
-import {AppRootStateType} from "./store";
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+export const initializeAppTC = createAsyncThunk('app/initializeAppTC', async (param, {dispatch, getState}) => {
+    const res = await authAPI.me()
+    if (res.data.resultCode === 0) {
+        dispatch(setIsLoggedInAC({value: true}));
+        // dispatch(setIsInitializedAC({isInitialized: true}));
+    } else {
+        // const isLoggedIn = getState().auth.isLoggedIn;
+        // if (isLoggedIn) {
+        //     handleServerAppError(dispatch, res.data);
+        // }
+    }
+    // .catch((e: AxiosError) => {
+    //     handleServerNetworkError(dispatch, e)
+    // }).finally(() => {
+    //     dispatch(setIsInitializedAC({isInitialized: true}));
+    // })
+})
 
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 export type ErrorType = null | string
@@ -29,35 +43,16 @@ const slice = createSlice({
             state.error = action.payload.error // immerjs
             // return {...state, error: action.error}
         },
-        setIsInitializedAC(state, action: PayloadAction<{isInitialized: boolean}>) {
-            state.isInitialized = action.payload.isInitialized // immerjs
-            // return {...state, isInitialized: action.isInitialized}
-        },
+    },
+    extraReducers: builder => {
+        builder.addCase(initializeAppTC.fulfilled, (state) => {
+            state.isInitialized = true
+        })
     }
 })
 
 export const appReducer = slice.reducer
-export const {setAppStatusAC, setAppErrorAC, setIsInitializedAC} = slice.actions
-
-export const initializeAppTC = () => (dispatch: Dispatch, getState: () => AppRootStateType) => {
-    authAPI.me()
-        .then(res => {
-            if (res.data.resultCode === 0) {
-                dispatch(setIsLoggedInAC({value: true}));
-                dispatch(setIsInitializedAC({isInitialized: true}));
-            } else {
-                const isLoggedIn = getState().auth.isLoggedIn;
-                if (isLoggedIn) {
-                    handleServerAppError(dispatch, res.data);
-                }
-            }
-        }).catch((e: AxiosError) => {
-            handleServerNetworkError(dispatch, e)
-        }).finally(() => {
-            dispatch(setIsInitializedAC({isInitialized: true}));
-        })
-}
+export const {setAppStatusAC, setAppErrorAC} = slice.actions
 
 export type SetAppStatusAСType = ReturnType<typeof setAppStatusAC>
 export type SetAppErrorAСType = ReturnType<typeof setAppErrorAC>
-export type SetAppInitializedAСType = ReturnType<typeof setIsInitializedAC>
